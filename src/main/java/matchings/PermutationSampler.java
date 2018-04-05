89d7758c13084192d8a0eaa7aa6fd0117ce641b8
G.Zhang <GongZ@dhcp-128-189-225-191.ubcsecure.wireless.ubc.ca>
2018-04-04 20:10:50 -0700

    Update Permutation Sampler

--- src/main/java/matchings/PermutationSampler.java
+++ src/main/java/matchings/PermutationSampler.java
@@ -34,24 +34,49 @@ public class PermutationSampler implements Sampler {
     // Fill this. 
 	// Refer to the file: Permutation.xtend 
 	
-	List<Integer> CurrentConnections = permutation.getConnections(); 
-	ArrayList<Integer> deepcopyCurrentConnections = new ArrayList<Integer>(CurrentConnections); 
+	// Current State 
+	List<Integer> CurrentState = permutation.getConnections(); 
+	ArrayList<Integer> deepcopyCurrentState = new ArrayList<Integer>(CurrentState); 
+//	System.out.println("Current State is: " + CurrentState); 
 	
+	// Find the log density of the current state 
 	final double CurrentDensity = logDensity(); 
 	// System.out.println(CurrentDensity); 
 	
-	// Perform "independence" Uniform sampling, which is available in Permutation.xtend 
-	permutation.sampleUniform(rand); 
-	List<Integer> NextConnections = permutation.getConnections(); 
+//	// Perform "independence" Uniform sampling, which is available in Permutation.xtend 
+//	permutation.sampleUniform(rand); 
+//	List<Integer> NextConnections = permutation.getConnections(); 
 	
+	// Make the swap of the randomly chosen 2 vertices in the current state 
+	int index1 = rand.nextInt(permutation.getConnections().size()); 
+	int index2 = rand.nextInt(permutation.getConnections().size()); // could be same to the previous one 
+//	System.out.println(index1); 
+//	System.out.println(index2); 
+	Collections.swap(permutation.getConnections(), index1, index2); 
+	
+	// Proposed State 
+	List<Integer> ProposedState = permutation.getConnections(); 
+//	System.out.println("Next State is: " + ProposedState); 
+	
+	// Find the log density of the proposed state 
 	final double NextDensity = logDensity(); 
 	// System.out.println(NextDensity); 
 	
-	final double probab = Math.min(1.0, Math.exp(NextDensity)/Math.exp(CurrentDensity)); 
-	boolean bern = Generators.bernoulli(rand, probab); 
-	if (!bern) {
-		for (int i = 0; i < deepcopyCurrentConnections.size(); i ++) { 
-			permutation.getConnections().set(i, deepcopyCurrentConnections.get(i)); 
+	final double Accept_Prob = Math.min(1.0, Math.exp(NextDensity)/Math.exp(CurrentDensity)); 
+	
+//	boolean bern = Generators.bernoulli(rand, Accept_Prob); 
+//	if (!bern) {
+//		for (int i = 0; i < deepcopyCurrentState.size(); i ++) { 
+//			permutation.getConnections().set(i, deepcopyCurrentState.get(i)); 
+//		} 
+//		// System.out.println("Next state is: \n \n \n" + permutation.getConnections()); 
+//	} 
+	
+	// Generate a Uniform [0, 1] random variable 
+	double u = Math.random();  
+	if (u>Accept_Prob) {
+		for (int i = 0; i < deepcopyCurrentState.size(); i ++) { 
+			permutation.getConnections().set(i, deepcopyCurrentState.get(i)); // Rejection step: recover to the previous state 
 		} 
 		// System.out.println("Next state is: \n \n \n" + permutation.getConnections()); 
 	} 
